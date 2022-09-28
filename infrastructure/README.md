4303c146759e557c4a0abe7e78f33abc1c6db086
zcemycl <yui.leung.15@ucl.ac.uk>
2022-09-28 09:49:00 +0100

    update plans for considering google cloud platform

--- infrastructure/README.md
+++ infrastructure/README.md
@@ -1,7 +1,16 @@
 ### Methods
-1. Github actions + Bicep (ghb)
-2. Github actions + Terraform (ght)
-3. Azure Devops + Bicep (adb)
-4. Azure Devops + Terraform (adt)
-5. Gitlab actions + Bicep (glb)
-6. Gitlab actions + Terraform (glt)
+1. Github actions + Bicep + Azure(ghb)
+    - `custom_ghb.yml`
+    - `deploy_ghb.yml`
+    - `docker.yml`
+2. Github actions + Terraform + AWS (ght)
+    - `deploy_ght.yml`
+3. Azure Devops + Bicep + Azure (adb)
+    - `custom_adb.yml`
+    - `deploy_adb.yml`
+4. Azure Devops + Terraform + Azure (adt)
+5. Gitlab CI + Bicep + Azure (glb)
+    - `cicd_glb.yml`
+6. Gitlab CI + Terraform + AWS (glt)
+7. Github actions + Terraform + GCP (ghtg)
+8. Gitlab CI + Terraform + GCP (gltg)
--- infrastructure/terraform/modules/aws/ecr/main.tf
+++ infrastructure/terraform/modules/aws/ecr/main.tf
@@ -2,26 +2,4 @@ resource "aws_ecr_repository" "this" {
     count = var.create?1:0
     name  = var.ecr_name
     force_delete = true
-    # provisioner "local-exec" {
-    #     command = <<-EOT
-    #         exec `echo ${aws_ecr_repository.this[0].arn} >> info.txt`
-    #         exec `echo ${aws_ecr_repository.this[0].registry_id} >> info.txt`
-    #         exec `echo ${aws_ecr_repository.this[0].repository_url} >> info.txt`
-    #     EOT
-    # }
 }
-
-# resource "null_resource" "push_docker_image" {
-#     provisioner "local-exec" {
-#         command = "aws ecr get-login-password --region ${var.AWS_REGION} | docker login --username AWS --password-stdin ${aws_ecr_repository.this.registry_id}.dkr.ecr.${var.AWS_REGION}.amazonaws.com >> info.txt"
-#     }
-#     provisioner "local-exec" {
-#         command = "docker tag dash:latest ${aws_ecr_repository.this.repository_url}:latest >> info.txt"
-#     }
-#     provisioner "local-exec" {
-#         command = "docker push ${aws_ecr_repository.this.repository_url}:latest >> info.txt"
-#     }
-#     depends_on = [
-#         aws_ecr_repository.this
-#     ]
-# }
