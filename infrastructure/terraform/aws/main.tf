587f76df2c255f52804cf7a5f5fc0d351f9d8769
zcemycl <yui.leung.15@ucl.ac.uk>
2022-09-29 19:11:37 +0100

    refractoring folders and try to deploy via github actions

--- infrastructure/terraform/aws/main.tf
+++ infrastructure/terraform/aws/main.tf
@@ -26,35 +26,35 @@ provider "aws" {
 }
 
 module "container_registry" {
-    source = "./modules/aws/ecr"
+    source = "./modules/ecr"
     create = true
     ecr_name = var.ecr_name
 }
 
 module "iam_ecs" {
-    source = "./modules/aws/iam"
+    source = "./modules/iam"
 }
 
 module "vpc" {
-    source = "./modules/aws/vpc"
+    source = "./modules/vpc"
     public_subnets = var.public_subnets
     availability_zones = var.availability_zones
 }
 
 module "sg" {
-    source = "./modules/aws/sg"
+    source = "./modules/sg"
     vpc_id = module.vpc.vpc_id
 }
 
 module "lb" {
-    source = "./modules/aws/lb"
+    source = "./modules/lb"
     subnets = module.vpc.public_subnet_ids
     vpc_id = module.vpc.vpc_id
     sg_id = module.sg.sg_lb_id
 }
 
 module "ecs" {
-    source = "./modules/aws/ecs"
+    source = "./modules/ecs"
     ecr_repo_url = module.container_registry.ecr_repo_url
     iam_ecs_task_exec_arn = module.iam_ecs.iam_ecs_task_exec_arn
     sg_service_id = module.sg.sg_service_id
@@ -65,7 +65,7 @@ module "ecs" {
 }
 
 module "autoscale" {
-    source = "./modules/aws/autoscale"
+    source = "./modules/autoscale"
     iam_ecs_agent_name = module.iam_ecs.iam_ecs_agent_name
     sg_service_id = module.sg.sg_service_id
     subnets = module.vpc.public_subnet_ids
